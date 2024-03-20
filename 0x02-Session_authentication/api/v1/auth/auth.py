d1e335af14d4cb37ac7b0c889b6b13570defe993
Ginika Elizabeth Nna <elizabethginika@gmail.com>
2024-03-20 06:06:17 -0700

    Session

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -1,31 +1,43 @@
 #!/usr/bin/env python3
-"""API authentication module."""
+"""
+manage API authentication
+"""
+
 
 from flask import request
 from typing import List, TypeVar
+from os import getenv
 
 
 class Auth:
-    """Authentication Object."""
+    """ manage API authentication """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Checks if API routes require authentication."""
-        if path is None or not excluded_paths:
+        """Check if path is in the list of excluded paths
+        """
+        if not path or not excluded_paths:
             return True
-        for i in excluded_paths:
-            if i.endswith('*') and path.startswith(i[:-1]):
-                return False
-            elif i in {path, path + '/'}:
+        if path[-1] != '/':
+            path += '/'
+        for p in excluded_paths:
+            if path[:p.find('*')] in p[:p.find('*')]:
                 return False
         return True
 
     def authorization_header(self, request=None) -> str:
-        """Checks if Authorization request header is present & valid."""
-        if request is None or "Authorization" not in request.headers:
+        """Return authorization header
+        """
+        if not request:
             return None
-        else:
-            return request.headers.get('Authorization')
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """Current user instance."""
+        """Return None
+        """
         return None
+
+    def session_cookie(self, request=None):
+        """Return a cookie value from a request
+        """
+        if request:
+            return request.cookies.get(getenv('SESSION_NAME'))
